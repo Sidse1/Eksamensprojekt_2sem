e1ee6ecbae1040cdda997efca09f7eb857b5f79a
Sidse1 <sidsebj1912@gmail.com>
2024-05-14 12:13:26 +0200

    Create menu.js

---
+++ JS/menu.js
@@ -0,0 +1,30 @@
+let sidebarWidth = 250;
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = sidebarWidth + "px";
+  document.getElementById("main2").style.marginRight = sidebarWidth + "px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main2").style.marginRight = "0";
+}
+
+let sidebarOpen = false;
+
+function toggleNav() {
+  if (sidebarOpen) {
+    closeNav();
+    sidebarOpen = false;
+  } else {
+    openNav();
+    sidebarOpen = true;
+  }
+}
+
+let buttons = document.querySelectorAll('.sidebar-button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', toggleNav);
+  toggleNav(); 
+});
